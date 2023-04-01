62b40d6424dcd5e02790b48e50798adedb4f1644
Jeferson-Js <oliveira.jeferson.jsogmail.com>
2023-04-01 13:33:07 -0300

    Atualização da aplicação.

--- DragDrop/js/scripts.js
+++ DragDrop/js/scripts.js
@@ -3,7 +3,6 @@ const dropzones = document.querySelectorAll(".dropzone");
 
 cards.forEach((card) => {
   card.addEventListener("dragstart", dragstart);
-  card.addEventListener("drag", drag);
   card.addEventListener("dragend", dragend);
 });
 
@@ -11,10 +10,6 @@ function dragstart() {
   dropzones.forEach((dropzone) => dropzone.classList.add("highlight"));
 
   this.classList.add("is-draging");
-}
-
-function drag() {
-  console.log("Drag");
 }
 
 function dragend() {
@@ -26,15 +21,11 @@ function dragend() {
 //place where we will drop cards
 
 dropzones.forEach((dropzone) => {
-  dropzone.addEventListener("dragenter", dragenter);
   dropzone.addEventListener("dragover", dragover);
   dropzone.addEventListener("dragleave", draleave);
   dropzone.addEventListener("drop", drop);
 });
 
-function dragenter() {
-  console.log("dragenter");
-}
 
 function dragover() {
   this.classList.add("over");
